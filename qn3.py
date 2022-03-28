f812158ae2f324e9345d42dcfe7b63bb1de355d8
johnwang <monkeygoessnap@gmail.com>
2022-03-28 17:55:14 +0800

    wk10

--- qn3.py
+++ qn3.py
@@ -1,9 +1,13 @@
 # Create another Python project, 
 # repeat the Question 2b and 2c from Laboratory Wk01
 
+# module class
 class Module:
+    # constructor
     def __init__(self, arg):
+        # assign arg to self.module
         self.module = arg
+        # define map of modulecode:modulename to reference
         self.moduleName = {
             "CSC1006":"Mathematics 2",
             "CSC1007":"Operating Systems",
@@ -11,17 +15,23 @@ class Module:
             "CSC1009":"Object-Oriented Programming",
             "CSC1010":"Computing Networks"
         }
+    # method to print module name    
     def printModuleName(self):
+        # try to access map for module code
         try:
             print(self.moduleName[self.module])
+        # if not print unknown module
         except:
             print("Unknown Module")
 
-
+# define main method
 def main():
-    m = Module("CSC10029")
+    # initiate object
+    m = Module("CSC1009")
+    #call method to print module name
     m.printModuleName()
 
+# check name and run main
 if __name__ == '__main__':
     main()
 
